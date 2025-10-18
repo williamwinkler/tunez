02167c6ca98ebb6e718d07a351de09cda601c469
William Winkler <williambwinkler@gmail.com>
2025-10-18 17:16:38 +0200

    chapter2 done

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -1,20 +1,33 @@
 defmodule Tunez.Music.Artist do
-  use Ash.Resource,
-    otp_app: :tunez,
-    domain: Tunez.Music,
-    data_layer: AshPostgres.DataLayer
+  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
 
   postgres do
     table "artists"
     repo Tunez.Repo
   end
 
+  actions do
+    defaults [:create, :read, :destroy]
+    default_accept [:name, :biography]
+
+    update :update do
+      require_atomic? false
+      accept [:name, :biography]
+
+      change Tunez.Music.Changes.UpdatePreviousNames, where: [changing(:name)]
+    end
+  end
+
   attributes do
     uuid_primary_key :id
 
     attribute :name, :string do
-      constraints [max_length: 255]
+      constraints max_length: 255
       allow_nil? false
+    end
+
+    attribute :previous_names, {:array, :string} do
+      default []
     end
 
     attribute :biography, :string
@@ -23,8 +36,9 @@ defmodule Tunez.Music.Artist do
     update_timestamp :updated_at
   end
 
-  actions do
-    defaults [:create, :read, :update, :destroy]
-    default_accept [:name, :biography]
+  relationships do
+    has_many :albums, Tunez.Music.Album do
+      sort year_released: :desc
+    end
   end
 end
